724e7d5df1076829126311d932de436829356c3b
James Heiniger <james.heiniger911@gmail.com>
2025-10-13 12:49:18 +1030

    Project complete with passwords can be copied by 1 click

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-test
+import {
+  fullArray,
+} from "/arrays.js";
+
+function random(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
+
+function arrSelector(arr) {
+  let retArr = [];
+  for (let i = 0; i < 15; i++) {
+    let randomArr = arr[random(arr)];
+    let chosenArr = randomArr[random(randomArr)];
+    let chosenChar = chosenArr[random(chosenArr)];
+    retArr.push(chosenChar);
+  }
+  return retArr;
+}
+
+let generate = document.getElementById('password-generator')
+let password1Box = document.getElementById('password1')
+let password2Box = document.getElementById('password2')
+
+generate.onclick = function() {
+  let password1 = arrSelector(fullArray)
+  let password2 = arrSelector(fullArray)
+  password1Box.textContent = password1.join('')
+  password2Box.textContent = password2.join('')
+}
+
+password1Box.onclick = function() {
+  navigator.clipboard.writeText(password1Box.innerText)
+  alert('password copied to clipboard')
+}
+
+password2Box.onclick = function() {
+  navigator.clipboard.writeText(password2Box.innerText)
+  alert('password copied to clipboard')
+}
+// input the arrays onto the screen using dom manipulation
+// to do this we need to create a button selector
+// when the button is clicked, we run the function twice, putting each
+// into it's respective box
